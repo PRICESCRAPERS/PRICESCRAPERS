b33352c2133ae6590c10967a8d1fdac1904cb501
maulen <jmaulena@ripley.com>
2023-07-20 18:35:47 -0400

    .

--- README.md
+++ README.md
@@ -1,16 +1,45 @@
-### Hi there 👋
+# priceScraper - Free Next.js priceScraper Website Template
 
-<!--
-**PRICESCRAPERS/PRICESCRAPERS** is a ✨ _special_ ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+priceScraper free, open-source, and premium-quality priceScraper website template for Next.js comes with everything you need to launch a priceScraper, business, or SaaS website, including all essential sections, components, and pages.
 
-Here are some ideas to get you started:
+If you're looking for a high-quality and visually appealing, feature-rich Next.js Template for your next priceScraper, SaaS, or business website, this is the perfect choice and starting point for you!
 
-- 🔭 I’m currently working on ...
-- 🌱 I’m currently learning ...
-- 👯 I’m looking to collaborate on ...
-- 🤔 I’m looking for help with ...
-- 💬 Ask me about ...
-- 📫 How to reach me: ...
-- 😄 Pronouns: ...
-- ⚡ Fun fact: ...
--->
+### ✨ Key Features
+
+- Crafted for priceScraper and SaaS Business
+- Next.js 13 and Tailwind CSS
+- All Essential Business Sections and Pages
+- High-quality and Clean Design
+- Dark and Light Version
+- TypeScript Support
+  and Much More ...
+
+### [🔥 Get priceScraper Pro](https://nextjstemplates.com/templates/saas-starter-priceScraper)
+
+[![priceScraper Pro](https://cdn.nextjstemplates.com/priceScraper-Pro---Next.js-Starter-Template-for-SaaS-priceScrapers-282e26f7-f543-4ae4-a777-ac306c08cce8.png)](https://nextjstemplates.com/templates/saas-starter-priceScraper)
+
+### [🚀 View Free Demo](https://priceScraper.nextjstemplates.com/)
+
+### [🚀 View Pro Demo](https://priceScraper-pro.nextjstemplates.com/)
+
+### [📦 Download](https://nextjstemplates.com/templates/priceScraper)
+
+### [🔥 Get Pro](https://nextjstemplates.com/templates/saas-starter-priceScraper)
+
+### [🔌 Documentation](https://nextjstemplates.com/docs)
+
+### ⚡ Deploy Now
+
+[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2FNextJSTemplates%2FpriceScraper-nextjs)
+
+[![Deploy with Netlify](https://www.netlify.com/img/deploy/button.svg)](https://app.netlify.com/start/deploy?repository=https://github.com/NextJSTemplates/priceScraper-nextjs)
+
+### 📄 License
+
+priceScraper is 100% free and open-source, feel free to use with your personal and commercial projects.
+
+### 💜 Support
+
+If you like the template, please star this repository to inspire the team to create more stuff like this and reach more users like you!
+
+### ✨ Explore and Download - Free [Next.js Templates](https://nextjstemplates.com)
---
+++ components/Contact/index.tsx
@@ -0,0 +1,86 @@
+import NewsLatterBox from './NewsLatterBox';
+
+const Contact = () => {
+  return (
+    <section id="contact" className="overflow-hidden py-16 md:py-20 lg:py-28">
+      <div className="container">
+        <div className="-mx-4 flex flex-wrap">
+          <div className="w-full px-4 lg:w-7/12 xl:w-8/12">
+            <div
+              className="wow fadeInUp mb-12 rounded-md bg-primary/[3%] py-11 px-8 dark:bg-dark sm:p-[55px] lg:mb-5 lg:px-8 xl:p-[55px]"
+              data-wow-delay=".15s
+              "
+            >
+              <h2 className="mb-3 text-2xl font-bold text-black dark:text-white sm:text-3xl lg:text-2xl xl:text-3xl">
+                Ponte en contacto
+              </h2>
+              <p className="mb-12 text-base font-medium text-body-color">
+                Nuestro equipo te respondera a la brevedad.
+              </p>
+              <form>
+                <div className="-mx-4 flex flex-wrap">
+                  <div className="w-full px-4 md:w-1/2">
+                    <div className="mb-8">
+                      <label
+                        htmlFor="name"
+                        className="mb-3 block text-sm font-medium text-dark dark:text-white"
+                      >
+                        Tu nombre
+                      </label>
+                      <input
+                        type="text"
+                        placeholder="Pon aqui tu nombre"
+                        className="w-full rounded-md border border-transparent py-3 px-6 text-base text-body-color placeholder-body-color shadow-one outline-none focus:border-primary focus-visible:shadow-none dark:bg-[#242B51] dark:shadow-signUp"
+                      />
+                    </div>
+                  </div>
+                  <div className="w-full px-4 md:w-1/2">
+                    <div className="mb-8">
+                      <label
+                        htmlFor="email"
+                        className="mb-3 block text-sm font-medium text-dark dark:text-white"
+                      >
+                        Tu correo
+                      </label>
+                      <input
+                        type="email"
+                        placeholder="Pon aqui tu correo electronico"
+                        className="w-full rounded-md border border-transparent py-3 px-6 text-base text-body-color placeholder-body-color shadow-one outline-none focus:border-primary focus-visible:shadow-none dark:bg-[#242B51] dark:shadow-signUp"
+                      />
+                    </div>
+                  </div>
+                  <div className="w-full px-4">
+                    <div className="mb-8">
+                      <label
+                        htmlFor="message"
+                        className="mb-3 block text-sm font-medium text-dark dark:text-white"
+                      >
+                        Escribenos tu mensaje
+                      </label>
+                      <textarea
+                        name="message"
+                        rows={5}
+                        placeholder="En que podemos ayudarte"
+                        className="w-full resize-none rounded-md border border-transparent py-3 px-6 text-base text-body-color placeholder-body-color shadow-one outline-none focus:border-primary focus-visible:shadow-none dark:bg-[#242B51] dark:shadow-signUp"
+                      ></textarea>
+                    </div>
+                  </div>
+                  <div className="w-full px-4">
+                    <button className="rounded-md bg-primary py-4 px-9 text-base font-medium text-white transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp">
+                      Enviar
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div className="w-full px-4 lg:w-5/12 xl:w-4/12">
+            <NewsLatterBox />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
